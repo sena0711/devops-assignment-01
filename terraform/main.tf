7aab05bfaa9725cc58989b18b26cb430686d6f81
Sena <sena0711@gmail.com>
2025-06-20 17:01:10 +0900

    errors fixed.

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,8 +37,8 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = false
 
-  vpc_id     = local.vpc_cidr
-  subnet_ids = local.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
