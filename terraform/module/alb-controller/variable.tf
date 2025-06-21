b079f802ee359c17686a3de6651b8f647c2fd2c7
Sena Kim <sena0711@gmail.com>
2025-06-22 00:47:18 +0900

    clustername not required on alb

--- terraform/module/alb-controller/variable.tf
+++ terraform/module/alb-controller/variable.tf
@@ -1,8 +1,19 @@
-variable "oidc_provider_arn" {}
-variable "oidc_provider_url" {}
+variable "oidc_provider_arn" {
+  description = "OIDC provider ARN for IRSA"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "OIDC provider URL (without https://)"
+  type        = string
+}
+
 variable "service_account_name" {
   default = "aws-load-balancer-controller"
 }
+
 variable "iam_role_name" {
+  description = "IAM role name for ALB Controller"
+  type        = string
   default = "AmazonEKSLoadBalancerControllerRole"
 }
