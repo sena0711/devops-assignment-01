81bf09a6d7b618fe5d15fe1cafbb78531c7277ee
Sena Kim <sena0711@gmail.com>
2025-06-22 01:13:33 +0900

    install alb controller

---
+++ terraform/alb-controller.tf
@@ -0,0 +1,25 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.13.0"
+
+  values = [
+    yamlencode({
+      clusterName = module.eks.cluster_name
+      region      = local.aws_region
+      vpcId       = module.vpc.vpc_id
+
+      serviceAccount = {
+        create = false
+        name   = "aws-load-balancer-controller"
+      }
+    })
+  ]
+
+  depends_on = [
+    module.alb_controller,
+    module.eks
+  ]
+}
