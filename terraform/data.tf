c7077cc51af8fd538a0e1b566f21535dfb427069
Sena Kim <sena0711@gmail.com>
2025-06-21 21:19:46 +0900

    add kube provider

--- terraform/data.tf
+++ terraform/data.tf
@@ -6,3 +6,11 @@ data "aws_availability_zones" "available" {
     values = ["opt-in-not-required"]
   }
 }
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.95, < 6.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 
   cloud {
@@ -21,6 +25,11 @@ provider "aws" {
   region = local.aws_region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
 
 variable "TFC_AWS_RUN_ROLE_ARN" {
   description = "ARN for the AWS role used by Terraform Cloud"
